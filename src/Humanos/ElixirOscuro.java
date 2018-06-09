d3905a56c8fb1459b3f1a3c7aa06d013acc76b30
JulioFlores <00081817@uca.edu.sv>
2018-06-09 17:42:37 -0600

    Agregando una edificacion mas

---
+++ src/Humanos/ElixirOscuro.java
@@ -0,0 +1,8 @@
+package Humanos;
+
+public class ElixirOscuro implements jugarHumanos{
+    @Override
+    public void jugar(){
+        System.out.println("Se comenzo a construir la mina del elixir oscuro, espere 1 turno");
+    }
+}
